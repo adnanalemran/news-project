126f88e353f564e34694dd09bfd6c8c7a5dba9a7
adnanalemran <adnanalemranontor@gmail.com>
2023-10-03 17:38:48 +0600

    Left side navigation

--- src/pages/sheard/leftSideNav/LeftSideNav.jsx
+++ src/pages/sheard/leftSideNav/LeftSideNav.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h2 className='text-2xl'>Left side nav</h2>
-            
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+  return (
+    <div className="space-y-6">
+      <h2 className="text-2xl">All Categories</h2>
+      {categories.map((category) => (
+        <Link className="block ml-4 text-xl font-semibold" 
+        to={`/category/${category.id}`}
+        key={category.id}>
+          {category.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default LeftSideNav;
