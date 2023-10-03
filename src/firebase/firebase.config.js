6db655d5273a0d0375884fb309d3be9885a9b80a
adnanalemran <adnanalemranontor@gmail.com>
2023-10-03 12:16:27 +0600

    Project Overview and Project setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdepgysTbkIGLqusJpFvzQPGG9dSQsIfM",
+  authDomain: "news-project-31777.firebaseapp.com",
+  projectId: "news-project-31777",
+  storageBucket: "news-project-31777.appspot.com",
+  messagingSenderId: "268046721553",
+  appId: "1:268046721553:web:857d837e61e6f9d09ec7b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
