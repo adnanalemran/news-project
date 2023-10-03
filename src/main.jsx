4a7d119e0eb4c2eb8719e3f0dd86a6e448dffaee
adnanalemran <adnanalemranontor@gmail.com>
2023-10-03 12:52:39 +0600

    React project Folder Structure and Router setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-]);
+import router from './routes/Routes';
+ 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-   <RouterProvider router={router} />
+   <RouterProvider router={router}/>
   </React.StrictMode>,
 )
