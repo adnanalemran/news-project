92502f46ef8cd041b965313452c31c34a5488f83
adnanalemran <adnanalemranontor@gmail.com>
2023-10-04 12:55:00 +0600

    Create Auth Context and integrate Register

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,57 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
- 
 const Login = () => {
-    return (
-        <div>
-            <Navbar/>
-            <h2 className="text-3xl">Login page</h2>
-        </div>
-    );
+    const handleLogin = e => {
+      e.preventDefault();
+      const form = new FormData(e.currentTarget);
+      console.log(form.get('password'));
+    }
+  
+
+  return (
+    <div>
+      <div className="font-poppins">
+        <Navbar />
+        <h2 className="text-center text-3xl">Login page</h2>
+        <form onSubmit={handleLogin} className="card-body md:w-3/4 lg:w-1/2 mx-auto">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              name="email"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              name='password'
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="text-center">Do not Have an account <Link  className='text-blue-400 font-bold' to='/register'>Registr</Link> </p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,109 @@
+ 
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const Register = () => {
+
+  const {createUser} = useContext(AuthContext);
+
+
+  const handleRegistration = e => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    const email = form.get('email');
+    const name = form.get('name');
+    const photo = form.get('photo');
+    const password =form.get('password')
+    console.log(email, password,name,photo);
+
+    //create user 
+    createUser (email,password)
+    .then(result => {
+      console.log(result);
+    })
+    .catch(error =>{
+      console.log(error);
+    }
+      
+      )
+ 
+  }
+  
+
+  return (
+    <div>
+      <div className="font-poppins">
+        <Navbar />
+        <h2 className="text-center text-3xl" tabIndex="0" aria-label="Register page">
+          Register page
+        </h2>
+        <form onSubmit={handleRegistration} className="card-body md:w-3/4 lg:w-1/2 mx-auto">
+          <div className="form-control">
+            <label htmlFor="name" className="label">
+            <span className="label-text">Name</span>
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Your name"
+              className="input input-bordered"
+              name="name"
+              required
+              aria-required="true"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="name" className="label">
+            <span className="label-text">Photo Url</span>
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Your Photo Url"
+              className="input input-bordered"
+              name="photo"
+              required
+              aria-required="true"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email" className="label">
+            <span className="label-text">Email</span>
+            </label>
+            <input
+              id="email"
+              type="email"
+              placeholder="email"
+              className="input input-bordered"
+              name="email"
+              required
+              aria-required="true"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="password" className="label">
+            <span className="label-text">Password</span>
+            </label>
+            <input
+              id="password"
+              type="password"
+              placeholder="password"
+              className="input input-bordered"
+              name='password'
+              required
+              aria-required="true"
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary" aria-label="Register">Register</button>
+          </div>
+        </form>
+        <p className="text-center">Already have an account? <Link className='text-blue-400 font-bold' to='/login'>Login</Link></p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
